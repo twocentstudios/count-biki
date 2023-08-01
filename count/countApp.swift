1078ea2f427a1db99a989f70d07b4ea7c6f76557
Christopher Trott <chris@twocentstudios.com>
2023-08-01 16:32:14 +0900

    Initial Commit

---
+++ count/countApp.swift
@@ -0,0 +1,17 @@
+//
+//  countApp.swift
+//  count
+//
+//  Created by Christopher Trott on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct countApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
