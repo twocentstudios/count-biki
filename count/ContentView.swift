a117fafbf396ec87895c574c37a4bd9ad5f3e125
Christopher Trott <chris@twocentstudios.com>
2023-08-03 00:36:54 +0900

    cleanup

--- count/ContentView.swift
+++ count/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  count
-//
-//  Created by Christopher Trott on 8/1/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- count/countApp.swift
+++ count/countApp.swift
@@ -1,10 +1,3 @@
-//
-//  countApp.swift
-//  count
-//
-//  Created by Christopher Trott on 8/1/23.
-//
-
 import SwiftUI
 
 @main
