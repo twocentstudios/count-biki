83145b54fed1ad4af5c3065bee8c457910c51018
Christopher Trott <chris@twocentstudios.com>
2023-08-10 15:57:08 +0900

    Initial ListeningQuizFeatureTests

---
+++ countTests/ListeningQuizFeatureTests.swift
@@ -0,0 +1,39 @@
+import ComposableArchitecture
+import XCTest
+
+@testable import count
+
+struct RandomNumberGeneratorWithSeed: RandomNumberGenerator {
+    init(seed: Int) {
+        srand48(seed)
+    }
+    
+    func next() -> UInt64 {
+        return withUnsafeBytes(of: drand48()) { bytes in
+            bytes.load(as: UInt64.self)
+        }
+    }
+}
+
+@MainActor final class ListeningQuizFeatureTests: XCTestCase {
+    func testOnAppear() async throws {
+        let speechExpectation = XCTestExpectation()
+        let store = TestStore(initialState: ListeningQuizFeature.State()) {
+            ListeningQuizFeature()
+        } withDependencies: {
+            $0.continuousClock = ImmediateClock()
+            $0.speechSynthesisClient.speak = { _ in speechExpectation.fulfill() }
+            $0.withRandomNumberGenerator = .init(RandomNumberGeneratorWithSeed(seed: 0))
+        }
+
+        await store.send(.onTask) {
+            $0.question = "2491"
+            $0.isSpeaking = true
+            $0.questionNumber = 1
+        }
+        
+        await store.receive(.onPlaybackFinished) {
+            $0.isSpeaking = false
+        }
+    }
+}
