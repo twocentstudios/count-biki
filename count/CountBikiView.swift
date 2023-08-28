47c51b677e7996665bd590d676d2002fe8126b8c
Christopher Trott <chris@twocentstudios.com>
2023-08-28 22:15:40 +0900

    Initial BikiView

---
+++ count/CountBikiView.swift
@@ -0,0 +1,71 @@
+import SceneKit
+import SwiftUI
+import ComposableArchitecture
+
+@MainActor struct CountBikiView: View {
+    let scene: SCNScene
+    let idlePlayer: SCNAnimationPlayer
+    let correctPlayer: SCNAnimationPlayer
+    let incorrectPlayer: SCNAnimationPlayer
+
+    init() {
+        let catalogName = "CountBiki.scnassets"
+        scene = .init(named: "\(catalogName)/count_biki_rig.scn")!
+        idlePlayer = Self.playerForAnimation(scene: scene, resourceName: "\(catalogName)/count_biki_anim_idle", isLooped: true)
+        correctPlayer = Self.playerForAnimation(scene: scene, resourceName: "\(catalogName)/count_biki_anim_correct", isLooped: false)
+        incorrectPlayer = Self.playerForAnimation(scene: scene, resourceName: "\(catalogName)/count_biki_anim_incorrect", isLooped: false)
+    }
+
+    var body: some View {
+        SceneView(
+            scene: scene,
+            pointOfView: nil,
+            options: [],
+            preferredFramesPerSecond: 24,
+            antialiasingMode: .multisampling4X,
+            delegate: nil,
+            technique: nil
+        )
+        .clipShape(Circle())
+        .aspectRatio(contentMode: .fit)
+        .onAppear {
+            idlePlayer.play()
+        }
+    }
+
+    static func playerForAnimation(scene: SCNScene, resourceName: String, isLooped: Bool = false) -> SCNAnimationPlayer {
+        guard let sceneURL = Bundle.main.url(forResource: resourceName, withExtension: "dae") else {
+            assertionFailure("\(resourceName).dae file not found")
+            return SCNAnimationPlayer()
+        }
+        guard let sceneSource = SCNSceneSource(url: sceneURL, options: nil) else {
+            assertionFailure("Scene for \(sceneURL) could not be loaded")
+            return SCNAnimationPlayer()
+        }
+
+        guard let animationKey = sceneSource.identifiersOfEntries(withClass: CAAnimation.self).first,
+              let animationGroup = sceneSource.entryWithIdentifier(animationKey, withClass: CAAnimation.self),
+              animationGroup.isKind(of: CAAnimationGroup.self)
+        else {
+            assertionFailure("Animation not found in \(resourceName)")
+            return SCNAnimationPlayer()
+        }
+
+        guard let targetNode = scene.rootNode.childNode(withName: "Armature", recursively: true) else {
+            assertionFailure("Armature node not found in scene")
+            return SCNAnimationPlayer()
+        }
+
+        animationGroup.repeatCount = isLooped ? .infinity : 1
+        animationGroup.fadeInDuration = 0.5
+        animationGroup.fadeOutDuration = 0.5
+
+        let player = SCNAnimationPlayer(animation: SCNAnimation(caAnimation: animationGroup))
+        targetNode.addAnimationPlayer(player, forKey: resourceName)
+        return player
+    }
+}
+
+#Preview {
+    CountBikiView()
+}
