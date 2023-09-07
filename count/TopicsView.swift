671e959687ddbba9916779ed38ee00ccb8ffe341
Christopher Trott <chris@twocentstudios.com>
2023-09-07 17:46:03 +0900

    Wrap NavigationStack at TopicView level

--- count/TopicsView.swift
+++ count/TopicsView.swift
@@ -60,73 +60,75 @@ struct TopicsView: View {
 
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
-            List {
-                Section {
-                    Text("No favorites yet")
-                        .font(.footnote)
-                        .foregroundStyle(.secondary)
-                        .multilineTextAlignment(.center)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .padding(.vertical, 20)
-                } header: {
-                    Text("Favorites \(Image(systemName: "star"))")
-                        .font(.headline)
-                        .foregroundStyle(.secondary)
-                        .textCase(nil)
-                }
-
-                Section {
-                    ForEach(viewStore.listeningCategories) { category in
-                        NavigationLink {
-                            List {
-                                Section {
-                                    ForEach(category.topics) { topic in
-                                        TopicCell(
-                                            title: topic.title,
-                                            subtitle: topic.description,
-                                            isFavorite: false,
-                                            tapped: { viewStore.send(.selectTopic(topic.id)) },
-                                            toggleFavoriteTapped: {}
-                                        )
-                                    }
-                                } footer: {
-                                    Text("Tip: tap and hold a topic to add/remove a favorite")
-                                }
-                            }
-                            .navigationBarTitleDisplayMode(.inline)
-                            .navigationTitle(category.title)
-                            .toolbar {
-                                ToolbarItem(placement: .principal) {
-                                    Text(category.title)
-                                        .font(.headline)
-                                }
-                            }
-                        } label: {
-                            VStack(alignment: .leading, spacing: 2) {
-                                Text(category.title).font(.headline)
-                                Text(category.description)
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-                            .padding(.vertical, 2)
-                        }
+            NavigationStack {
+                List {
+                    Section {
+                        Text("No favorites yet")
+                            .font(.footnote)
+                            .foregroundStyle(.secondary)
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.vertical, 20)
+                    } header: {
+                        Text("Favorites \(Image(systemName: "star"))")
+                            .font(.headline)
+                            .foregroundStyle(.secondary)
+                            .textCase(nil)
                     }
-                } header: {
-                    Text("Listening \(Image(systemName: "ear"))")
-                        .font(.headline)
-                        .foregroundStyle(.secondary)
-                        .textCase(nil)
-                } footer: {
-                    Text("Listen to a clip and transcribe the number")
+                    
+                    Section {
+                        ForEach(viewStore.listeningCategories) { category in
+                            NavigationLink {
+                                List {
+                                    Section {
+                                        ForEach(category.topics) { topic in
+                                            TopicCell(
+                                                title: topic.title,
+                                                subtitle: topic.description,
+                                                isFavorite: false,
+                                                tapped: { viewStore.send(.selectTopic(topic.id)) },
+                                                toggleFavoriteTapped: {}
+                                            )
+                                        }
+                                    } footer: {
+                                        Text("Tip: tap and hold a topic to add/remove a favorite")
+                                    }
+                                }
+                                .navigationBarTitleDisplayMode(.inline)
+                                .navigationTitle(category.title)
+                                .toolbar {
+                                    ToolbarItem(placement: .principal) {
+                                        Text(category.title)
+                                            .font(.headline)
+                                    }
+                                }
+                            } label: {
+                                VStack(alignment: .leading, spacing: 2) {
+                                    Text(category.title).font(.headline)
+                                    Text(category.description)
+                                        .font(.caption)
+                                        .foregroundStyle(.secondary)
+                                }
+                                .padding(.vertical, 2)
+                            }
+                        }
+                    } header: {
+                        Text("Listening \(Image(systemName: "ear"))")
+                            .font(.headline)
+                            .foregroundStyle(.secondary)
+                            .textCase(nil)
+                    } footer: {
+                        Text("Listen to a clip and transcribe the number")
+                    }
                 }
-            }
-            .listStyle(.automatic)
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationTitle("Topics")
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("Topics")
-                        .font(.headline)
+                .listStyle(.automatic)
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationTitle("Topics")
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("Topics")
+                            .font(.headline)
+                    }
                 }
             }
         }
