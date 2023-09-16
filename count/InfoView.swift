a0d6d1a08a56d02fff5bb82822f1e75342d5f641
Christopher Trott <chris@twocentstudios.com>
2023-09-16 16:01:44 +0900

    Add InfoView

---
+++ count/InfoView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        List {
+            Text("hello")
+        }
+    }
+}
+
+#Preview {
+    InfoView()
+        .fontDesign(.rounded)
+}
