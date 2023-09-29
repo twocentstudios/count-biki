050e435186f632d457ce2945a92e8e79a28da1c1
Christopher Trott <chris@twocentstudios.com>
2023-09-29 21:04:13 +0900

    animation

--- count/Feature/TranslyvaniaTierFeature.swift
+++ count/Feature/TranslyvaniaTierFeature.swift
@@ -95,39 +95,28 @@ struct TranslyvaniaTierView: View {
             ScrollView {
                 VStack(spacing: 20) {
                     VStack(spacing: 10) {
-                        // TODO: transitions
+                        Text(viewStore.hasTranslyvaniaTier ? "You've unlocked..." : "Leave any size tip to unlock...")
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                        Text("Translyvania Tier")
+                            .font(.largeTitle)
+                            .fontWeight(.black)
+                            .foregroundStyle(LinearGradient(colors: [Color.red, Color.orange, Color.yellow], startPoint: .bottomLeading, endPoint: .topTrailing))
                         if viewStore.hasTranslyvaniaTier {
-                            Text("You've unlocked...")
-                                .font(.subheadline)
-                                .foregroundStyle(.secondary)
-                            Text("Translyvania Tier")
-                                .font(.largeTitle)
-                                .fontWeight(.black)
-                                .foregroundStyle(LinearGradient(colors: [Color.red, Color.orange, Color.yellow], startPoint: .bottomLeading, endPoint: .topTrailing))
                             Text("Thanks for supporting development")
                                 .fontDesign(.default)
                                 .italic()
-                            Text("If you find Count Biki useful, you're welcome to leave another tip :)")
-                                .font(.footnote)
-                                .multilineTextAlignment(.center)
-                        } else {
-                            Text("Leave any size tip to unlock...")
-                                .font(.subheadline)
-                                .foregroundStyle(.secondary)
-                            Text("Translyvania Tier")
-                                .font(.largeTitle)
-                                .fontWeight(.black)
-                                .foregroundStyle(LinearGradient(colors: [Color.red, Color.orange, Color.yellow], startPoint: .bottomLeading, endPoint: .topTrailing))
-                            VStack(alignment: .leading, spacing: 10) {
-                                Text("\(Image(systemName: "1.circle"))  Choose your favorite app icon")
-                                Text("\(Image(systemName: "2.circle"))  Support ongoing development")
-                            }
-                            .font(.headline)
-                            .padding(.trailing, 20)
-                            .padding(.vertical, 16)
-                            .frame(maxWidth: .infinity, alignment: .center)
                         }
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("\(Image(systemName: "1.circle"))  Choose your favorite app icon")
+                            Text("\(Image(systemName: "2.circle"))  Support ongoing development")
+                        }
+                        .font(.headline)
+                        .padding(.trailing, 20)
+                        .padding(.vertical, 16)
+                        .frame(maxWidth: .infinity, alignment: .center)
                     }
+                    .animation(.default, value: viewStore.hasTranslyvaniaTier)
                     if viewStore.availableProducts.isLoading {
                         ProgressView().padding()
                     }
